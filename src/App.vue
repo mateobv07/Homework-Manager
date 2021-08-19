1f3200187f4909294802f5c45cdec1437b2cffa5
unknown <bernasconivargas.mateo@gmail.com>
2021-08-18 22:27:06 -0500

    cambios navbar

--- src/App.vue
+++ src/App.vue
@@ -2,35 +2,21 @@
   <v-app>
     <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
+        <h1 class="text-h4">Homework Tec</h1>
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="../src/assets/tec.png"
+          transition="scale-transition"
+          width="140"
+        />
     </v-app-bar>
 
     <v-main>
